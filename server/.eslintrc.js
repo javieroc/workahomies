38c648797f6b16d386f56280ec70c2201526b93f
Javier Ocampo <ferroxido@gmail.com>
2021-07-09 10:24:00 -0300

    Setup project :rocket:

---
+++ server/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    eqeqeq: 'error',
+    'no-console': 'warn',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+};
